676dfbbe03aa99b77fc46abc23093087483f7b6a
Miroslav <mspehar@gmail.com>
2018-07-15 22:41:56 +0200

    more ui tweaks, margins

--- rupicapra/ConnectionManagerDialog.xaml.cs
+++ rupicapra/ConnectionManagerDialog.xaml.cs
@@ -23,5 +23,10 @@ namespace chamois
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
